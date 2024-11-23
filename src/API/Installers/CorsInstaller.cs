d54ea85746f0396c239421961ad84ad4772663a2
Scooby <mateusz.skubiszak1@wp.pl>
2024-11-23 20:14:05 +0100

    Add cors

---
+++ src/API/Installers/CorsInstaller.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Installers
+{
+    public class CorsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.WithOrigins("http://localhost:5173", "https://localhost:5173")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials();
+                });
+            });
+        }
+    }
+}
